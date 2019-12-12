4319db57f821ea6d6d0140f2fe6bd27315b49bf6
LiamLugh <liamlughhouston@gmail.com>
2019-12-12 15:23:18 +1100

    Created floor and collider prefabs, added Map data scripts

---
+++ MK2020/Assets/Scripts/MapBuffer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapBuffer : MonoBehaviour
+{
+    public GameObject pfb;
+    [SerializeField]
+    byte[,] mapData;
+    [SerializeField]
+    byte width = 0;
+    [SerializeField]
+    byte height = 0;
+
+    void Start()
+    {
+        float heightf = Camera.main.orthographicSize * 2.0f;
+        float widthf = heightf * Camera.main.aspect;
+
+        width = (byte)Mathf.CeilToInt(widthf);
+        height = (byte)Mathf.CeilToInt(heightf);
+
+        mapData = new byte[width, height];
+
+        for(int x = 0; x < width; x++)
+        {
+            Instantiate(pfb, new Vector2(x, height), Quaternion.identity, transform);
+        }
+
+        for (int y = 0; y < height; y++)
+        {
+            Instantiate(pfb, new Vector2(width, y), Quaternion.identity, transform);
+        }
+    }
+}
--- MK2020/Assets/Scripts/Player.cs
+++ MK2020/Assets/Scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : MonoBehaviour
     [SerializeField]
     Material[] mats;
     bool isWhiteMode = true;
+    float halfScreenWidth = 0.0f;
     bool isPaused = false;
     [SerializeField]
     float jumpPower = 0.0f;
@@ -30,6 +31,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         childrenCount = childRenderers.Length;
+        halfScreenWidth = Screen.width / 2;
         jumpPower = minPower;
     }
 
@@ -37,21 +39,23 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.LeftControl) && !isWhiteMode)
         {
-            myRenderer.material = mats[0];
-            for (int i = 0; i < childrenCount; i++)
-            {
-                childRenderers[i].material = mats[1];
-            }
-            isWhiteMode = true;
+            SetWhite();
         }
         else if (Input.GetKeyDown(KeyCode.RightControl) && isWhiteMode)
         {
-            myRenderer.material = mats[2];
-            for (int i = 0; i < childrenCount; i++)
+            SetBlack();
+        }
+        else if (Input.touchCount > 0)
+        {
+            Touch t = Input.GetTouch(0);
+            if (t.position.x < halfScreenWidth && !isWhiteMode)
             {
-                childRenderers[i].material = mats[3];
+                SetWhite();
             }
-            isWhiteMode = false;
+            else if (t.position.x > halfScreenWidth && isWhiteMode)
+            {
+                SetBlack();
+            }
         }
     }
 
@@ -61,7 +65,7 @@ public class Player : MonoBehaviour
         {
             if(floorCollider.GetCanJump())
             {
-                if(!Input.anyKey && jumpPower > minPower)
+                if((!Input.anyKey && Input.touchCount == 0) && jumpPower > minPower)
                 {
                     rb.velocity.Set(0.0f, 0.0f);
                     jumpVector.Set(0.0f, jumpPower);
@@ -70,7 +74,7 @@ public class Player : MonoBehaviour
                 }
             }
 
-            if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+            if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) || Input.touchCount > 0)
             {
                 jumpPower += powerMultiplier * Time.fixedDeltaTime;
 
@@ -84,5 +88,25 @@ public class Player : MonoBehaviour
                 jumpPower = 0.0f;
             }
         }
+    }
+
+    void SetBlack()
+    {
+        myRenderer.material = mats[2];
+        for (int i = 0; i < childrenCount; i++)
+        {
+            childRenderers[i].material = mats[3];
+        }
+        isWhiteMode = false;
+    }
+
+    void SetWhite()
+    {
+        myRenderer.material = mats[0];
+        for (int i = 0; i < childrenCount; i++)
+        {
+            childRenderers[i].material = mats[1];
+        }
+        isWhiteMode = true;
     }
 }
