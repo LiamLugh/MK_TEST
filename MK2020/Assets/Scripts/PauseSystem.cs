b36b50ac8ce8cd25b8681e6150a3ac3b54257deb
LiamLugh <liamlughhouston@gmail.com>
2019-12-14 16:40:42 +1100

    Added Pause System and UI to match

---
+++ MK2020/Assets/Scripts/PauseSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseSystem : MonoBehaviour
+{
+    [SerializeField]
+    Player p = null;
+    [SerializeField]
+    MapController m = null;
+
+    public void PauseGame()
+    {
+        //p.Pause();
+        //m.Pause();
+    }
+
+}
--- MK2020/Assets/Scripts/ScoringSystem.cs
+++ MK2020/Assets/Scripts/ScoringSystem.cs
@@ -1,8 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoringSystem : MonoBehaviour
 {
+    [SerializeField]
+    uint currentScore = 0;
+    [SerializeField]
+    uint currentHighScore = 0;
+
     // UI references
+    [SerializeField]
+    Text scoreText;
+    [SerializeField]
+    Text highScoreText;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        UpdateScoreText(currentScore);
+        UpdateHighScoreText(currentHighScore);
+    }
+
+    void UpdateScoreText(uint score)
+    {
+        scoreText.text = score.ToString();
+    }
+
+    void UpdateHighScoreText(uint score)
+    {
+        if(score < 1)
+        {
+            highScoreText.gameObject.SetActive(false);
+            return;
+        }
+
+        highScoreText.text = score.ToString();
+    }
 }
